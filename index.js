7e71e67a02e61f6b9d64693465eb54728ba01155
Happiness11 <happiness@settl.me>
2022-11-05 02:30:52 +0100

    added post request

--- index.js
+++ index.js
@@ -2,15 +2,42 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const host = "0.0.0.0";
+
+app.use(express.json());
+
+const slackUsername = "Tana00";
 
 // Defining get request at '/' route
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.json({
-    slackUsername: "Tana00",
+    slackUsername,
     backend: true,
     age: 29,
     bio: "Hi, I am Happiness, a tech and food lover. Nice to meet you!",
+  });
+});
+
+// Defining post request at '/post' route
+app.post("/post", (req, res) => {
+  const operation_type = req.body.operation_type;
+  let x = Number(req.body.x);
+  let y = Number(req.body.y);
+  let result;
+
+  if (operation_type === "addition") {
+    result = x + y;
+  }
+  if (operation_type === "subtraction") {
+    result = x - y;
+  }
+  if (operation_type === "multiplication") {
+    result = x * y;
+  }
+
+  res.send({
+    slackUsername,
+    operation_type: req.body.operation_type,
+    result,
   });
 });
 
